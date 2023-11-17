1606c65714d2656f5b76f766296a4b9153abad64
alshimaamamdouh <alshimaa.mamdouh.abdelaziz@gmail.com>
2023-11-16 18:06:36 -0800

    update

--- add.c
+++ add.c
@@ -7,21 +7,9 @@
 */
 void _add(stack_t **stack, unsigned int line_number)
 {
-int add_res = 0, len;
-stack_t *node, *temp;
+int add_res = 0;
+stack_t *node;
 if (*stack == NULL || (*stack)->next == NULL)
-{
-fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
-free_stack(*stack);
-exit(EXIT_FAILURE);
-}
-temp = *stack;
-while (temp)
-{
-temp = temp->next;
-len++;
-}
-if (len < 2)
 {
 fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
 free_stack(*stack);
@@ -32,5 +20,4 @@ node = *stack;
 node->next->n = add_res;
 *stack = node->next;
 free(node);
-free(temp);
 }
