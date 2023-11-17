401e0e378f90bfca0b481fd5024352a8770e6c4a
alshimaamamdouh <alshimaa.mamdouh.abdelaziz@gmail.com>
2023-11-16 18:42:04 -0800

    sub

--- sub.c
+++ sub.c
@@ -7,7 +7,7 @@
 */
 void _sub(stack_t **stack, unsigned int line_number)
 {
-int sub_res = 0, i, j;
+int sub_res = 0;
 stack_t *node;
 if (*stack == NULL || (*stack)->next == NULL)
 {
@@ -15,14 +15,7 @@ fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
 free_stack(*stack);
 exit(EXIT_FAILURE);
 }
-i = (*stack)->next->n;
-j = (*stack)->n;
-if (i > j)
-sub_res = i - j;
-
-else
-sub_res = j - i;
-
+sub_res = (*stack)->next->n - (*stack)->n;
 node = *stack;
 node->next->n = sub_res;
 *stack = node->next;
