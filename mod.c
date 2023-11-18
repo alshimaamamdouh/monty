35568ede5d9df67010724f858fb9886de79a3db0
alshimaamamdouh <alshimaa.mamdouh.abdelaziz@gmail.com>
2023-11-17 18:07:02 -0800

    m

--- mod.c
+++ mod.c
@@ -1,23 +1,23 @@
 #include "monty.h"
 /**
- * _div - div elements
+ * _mod - mod elements
  * @stack: input pointer
  * @line_number: line number
  *
 */
-void _div(stack_t **stack, unsigned int line_number)
+void _mod(stack_t **stack, unsigned int line_number)
 {
-int div_res = 0;
+int mod_res = 0;
 stack_t *node;
 if (*stack == NULL || (*stack)->next == NULL || (*stack)->n == 0)
 {
-fprintf(stderr, "L%d: can't div, stack too short\n", line_number);
+fprintf(stderr, "L%d: can't mod, stack too short\n", line_number);
 free_stack(*stack);
 exit(EXIT_FAILURE);
 }
-div_res = ((*stack)->next->n) / ((*stack)->n);
+mod_res = ((*stack)->next->n) % ((*stack)->n);
 node = *stack;
-node->next->n = div_res;
+node->next->n = mod_res;
 *stack = node->next;
 free(node);
 }
