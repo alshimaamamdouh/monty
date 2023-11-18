12ace514a44e6fb947c6cbc6ccebb587dfe6edce
alshimaamamdouh <alshimaa.mamdouh.abdelaziz@gmail.com>
2023-11-17 16:21:29 -0800

    update

--- push.c
+++ push.c
@@ -8,8 +8,6 @@ void _push(stack_t **stack, unsigned int line_number)
 {
 int n, j = 0, flag = 0;
 
-if (bus.arg)
-{
 for (; bus.arg[j] != '\0'; j++)
 {
 if (bus.arg[j] > 57 || bus.arg[j] < 48)
@@ -24,10 +22,8 @@ fprintf(stderr, "L%d: usage: push integer\n", line_number);
 fclose(bus.file);
 free(bus.content);
 free_stack(*stack);
-exit(EXIT_FAILURE); }
-
-
-
+exit(EXIT_FAILURE); 
+}
 else
 {
 
@@ -38,9 +34,6 @@ free(bus.content);
 free_stack(*stack);
 exit(EXIT_FAILURE); 
 }
-}
-else
-exit(EXIT_FAILURE);
 
 n = atoi(bus.arg);
 _addnode(stack, n);
